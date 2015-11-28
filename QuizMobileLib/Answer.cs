c2e9cd45b37342fad97142a04884da2235ffeb5b
OceanOSdev <ThomasTNF@live.com>
2015-11-27 21:11:19 -0600

    implemented IComparable and IEquatable interfaces for "answer"

--- QuizMobileLib/Answer.cs
+++ QuizMobileLib/Answer.cs
@@ -1,24 +1,60 @@
-﻿namespace QuizMobileLib
+﻿using System;
+
+namespace QuizMobileLib
 {
     /// <summary>
     /// Contains the answer for a specific question.
     /// </summary>
-    public class Answer
+    public class Answer : IComparable<Answer>, IEquatable<Answer>
     {
+        #region Property
+        /// <summary>
+        /// The string representation of the answer.
+        /// </summary>
+        public string Text { get; set; }
+        #endregion
+
+        #region Constructor
         /// <summary>
         /// Initialize an <code>Answer</code> object
         /// </summary>
         /// <param name="answer">The string representation of the answer.</param>
         public Answer(string answer)
         {
-            Text = answer;
+            Text = answer;   
         }
+        #endregion
 
+        #region Interface Implemetations
+        #region IComparable<Answer>
         /// <summary>
-        /// The string representation of the answer.
+        /// Compares this instance to a specified Answer and returns an indication
+        /// of their relative values.
         /// </summary>
-        public string Text { get; set; }
-
+        /// <param name="other">An Answer to compare.</param>
+        /// <returns>
+        /// A signed number indicating the relative values of this instance and value.Return
+        /// Value Description Less than zero This instance is less than value. Zero This
+        /// instance is equal to value. Greater than zero This instance is greater than value.
+        /// </returns>
+        public int CompareTo(Answer other)
+        {
+            return Text.CompareTo(other.Text);
+        }
+        #endregion
+        #region IEquatable<Answer>
+        /// <summary>
+        /// Determines if this instance contains the same value as the compared one.
+        /// </summary>
+        /// <param name="other">The answer to compare.</param>
+        /// <returns>True if the instances' values are equal, otherwise false.</returns>
+        public bool Equals(Answer other)
+        {
+            return Text == other.Text;
+        }
+        #endregion
+        #endregion
+        #region Operators
         /// <summary>
         /// Allows you to instatiate an answer by just providing the string.
         /// </summary>
@@ -33,6 +69,55 @@
         {
             return new Answer(val);
         }
+
+        /// <summary>
+        /// Overrided "==" operator.
+        /// </summary>
+        /// <param name="lhs">Left hand side.</param>
+        /// <param name="rhs">Right hand side.</param>
+        /// <returns></returns>
+        public static bool operator ==(Answer lhs, Answer rhs) => lhs.Equals(rhs);
+
+        /// <summary>
+        /// Overrided "!=" operator
+        /// </summary>
+        /// <param name="lhs">Left hand side.</param>
+        /// <param name="rhs">Right hand side.</param>
+        /// <returns></returns>
+        public static bool operator !=(Answer lhs, Answer rhs) => !lhs.Equals(rhs);
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Determines whether the specified object is equal to the current one.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            
+            if (obj is Answer)
+                return Equals(obj as Answer);
+            else
+                return false;
+            
+        }
+
+        /// <summary>
+        /// Returns hash code for this instance.
+        /// </summary>
+        /// <returns>The hash code for this instance.</returns>
+        public override int GetHashCode()
+        {
+            // Temporary solution to generate hash code
+            return Text.GetHashCode();
+        }
+        #endregion
 
     }
 }
--- QuizMobileLib/Question.cs
+++ QuizMobileLib/Question.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace QuizMobileLib
 {
@@ -12,8 +14,7 @@ namespace QuizMobileLib
     /// </summary>
     public class Question
     {
-        #region Properties
-        
+        #region Properties  
         /// <summary>
         /// The Universal Resource Identifier for the image (if there is one).
         /// </summary>
@@ -32,11 +33,11 @@ namespace QuizMobileLib
         /// <summary>
         /// The correct answer to the question.
         /// </summary>
+        [Required(ErrorMessage = "For any question, there must be at least one correct answer.")]
         public Answer CorrectAnswer { get; set; }
         #endregion
 
         #region Constructor
-
         /// <summary>
         /// Initialize an empty question.
         /// </summary>
@@ -48,7 +49,29 @@ namespace QuizMobileLib
         }
 
         /// <summary>
-        /// Initialize a question with an image
+        /// Initialize a question.
+        /// </summary>
+        /// <param name="text">The string that represents the question.</param>
+        public Question(string text)
+        {
+            ImageURI = string.Empty;
+            Text = text;
+        }
+
+        /// <summary>
+        /// Initialize a question with a preset correct answer.
+        /// </summary>
+        /// <param name="text">The string that represents the question.</param>
+        /// <param name="correctAnswer">The correct answer to the question.</param>
+        public Question(string text, Answer correctAnswer)
+        {
+            ImageURI = string.Empty;
+            Text = text;
+            CorrectAnswer = correctAnswer;
+        }
+
+        /// <summary>
+        /// Initialize a question with an image.
         /// </summary>
         /// <param name="imageUri">The Universal Resource Identifier for the image.</param>
         /// <param name="text">The string that represents the question.</param>
@@ -59,6 +82,19 @@ namespace QuizMobileLib
             Text = text;
             CorrectAnswer = correctAnswer;
         }
+        #endregion
+
+        #region Operators
+        /// <summary>
+        /// Allows you to instantiate a <see cref="Question"/> by only providing the string.
+        /// </summary>
+        /// <param name="val">The string that represents the question.</param>
+        /// <remarks>This is only provided for convenience, recommended that you instantiate the proper way.</remarks>
+        public static implicit operator Question(string val) => new Question(val);
+        #endregion
+
+        #region Methods
+
         #endregion
     }
 }
